6dbf3c278322104c90ef5f6d42b584b6ce657309
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2015-09-21 17:52:27 +0300

    Initial commit

---
+++ app/src/main/java/com/kurkov/p0781_tabcontentfactory/MainActivity.java
@@ -0,0 +1,47 @@
+package com.kurkov.p0781_tabcontentfactory;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+public class MainActivity extends TabActivity {
+
+    final String TABS_TAG_1 = "Tag 1";
+    final String TABS_TAG_2 = "Tag 2";
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpec;
+
+        tabSpec = tabHost.newTabSpec(TABS_TAG_1);
+        tabSpec.setContent(TabFactory);
+        tabSpec.setIndicator("Вкладка 1");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec(TABS_TAG_2);
+        tabSpec.setContent(TabFactory);
+        tabSpec.setIndicator("Вкладка 2");
+        tabHost.addTab(tabSpec);
+    }
+
+    TabHost.TabContentFactory TabFactory = new TabHost.TabContentFactory() {
+
+        @Override
+        public View createTabContent(String tag) {
+            if (tag == TABS_TAG_1) {
+                return getLayoutInflater().inflate(R.layout.tab, null);
+            } else if (tag == TABS_TAG_2) {
+                TextView tv = new TextView(MainActivity.this);
+                tv.setText("Это создано вручную");
+                return tv;
+            }
+            return null;
+        }
+    };
+}
